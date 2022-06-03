eaa77279c73073fe9de31b780b3bcdf9ae36c7f4
Nazneen Oomatia <naz.chini@gmail.com>
2022-06-03 11:55:15 +0200

    Remove board actions from profile page

--- frontend/src/views/profile.vue
+++ frontend/src/views/profile.vue
@@ -1,15 +1,15 @@
 <script>
 // @ is an alias to /src
 import { mapActions, mapState } from 'vuex'
-import Board from '../components/board'
-import Card from '../components/card'
+// import Board from '../components/board'
+// import Card from '../components/card'
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Profile',
   components: {
-    Board,
-    Card,
+    // Board,
+    // Card,
   },
   data() {
     return {
@@ -55,34 +55,34 @@ export default {
 
 <template lang="pug">
   .home 
-    h1 zalook 
-      span(v-if="user") {{ user.firstName }}
+    //- h1 zalook 
+    //-   span(v-if="user") {{ user.firstName }}
     
-    main.flexbox
-      Board#board-1
-        Card#card-1(draggable="true")
-          p Card 1
+    //- main.flexbox
+    //-   Board#board-1
+    //-     Card#card-1(draggable="true")
+    //-       p Card 1
       
-      Board#board-2
-        Card#card-2(draggable="true")
-          p Card 2
-    //- h2 Users
-    //- div(v-for="user in users")
-    //-   router-link(:to="`/users/${user._id}`") {{user.firstName}}
-    //- div(v-if="liveStreams.length")
-    //-   h2 Live streams
-    //-   div(v-for="stream in liveStreams")
-    //-     p {{ stream }}
-    //-     button(@click="joinStream(stream)") Join stream
-    //- button(@click="goLive") Go live
-    //- div(v-if="currentLiveStream")
-    //-   h3 Live stream
-    //-   .messages
-    //-     .message(v-for="message in liveStreamMessages")
-    //-       p
-    //-         span {{ message.author }}:&nbsp;
-    //-         span {{ message.body }}
-    //-   form(@submit="sendMessage")
-    //-     input(type="text" v-model="message")
-    //-     input(type="submit" value="Send message")
+    //-   Board#board-2
+    //-     Card#card-2(draggable="true")
+    //-       p Card 2
+    h2 Users
+    div(v-for="user in users")
+      router-link(:to="`/users/${user._id}`") {{user.firstName}}
+    div(v-if="liveStreams.length")
+      h2 Live streams
+      div(v-for="stream in liveStreams")
+        p {{ stream }}
+        button(@click="joinStream(stream)") Join stream
+    button(@click="goLive") Go live
+    div(v-if="currentLiveStream")
+      h3 Live stream
+      .messages
+        .message(v-for="message in liveStreamMessages")
+          p
+            span {{ message.author }}:&nbsp;
+            span {{ message.body }}
+      form(@submit="sendMessage")
+        input(type="text" v-model="message")
+        input(type="submit" value="Send message")
 </template>
