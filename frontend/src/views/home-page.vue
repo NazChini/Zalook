6aa6b7e2c5228e3b28fc1c93733671b611e5ad22
Nazneen Oomatia <naz.chini@gmail.com>
2022-06-03 10:12:47 +0200

    Create board

---
+++ frontend/src/views/home-page.vue
@@ -0,0 +1,56 @@
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Home-Page',
+  methods: {
+    ...mapActions(['logout']),
+    async doLogout() {
+      await this.logout()
+      this.$router.push('/login')
+    },
+  },
+}
+</script>
+
+<template lang="pug">
+#nav
+    router-link(to="/profile") Profile
+    router-link(to="/login") Login
+    router-link(to="/register") Register
+    a(@click="doLogout" href="#") Logout
+router-view
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f3f3f3;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
