90601ef79229f32e04a4663294cf5ab786d3bc87
Nazneen Oomatia <naz.chini@gmail.com>
2022-06-20 21:42:39 +0200

    Fix default background color of all pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,12 +11,7 @@ export default {
 
 <style lang="scss">
 @import './assets/styles/reset.scss';
-
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: #fff;
 }
 </style>
